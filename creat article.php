a6ab96e4225ef1bed1f37fbc394cd424dededc4e
Cosmolog1 <bourret57@gmail.com>
2025-02-25 16:39:30 +0100

    Brief_Blog_Creation_v0.2

--- creat article.php
+++ creat article.php
@@ -3,13 +3,47 @@ include_once("component/header.php");
 ?>
 
 
-<h1>Creat article</h1>
+<h2>Creat article</h2>
+
+<div>
+    <br>
+    <label for="title">Title :</label>
+    <input id="title" type="text" name="titre" required>
+
+    <br><br><label for="pseudo">Pseudo :</label>
+    <input type="text" name="pseudo" required>
+
+    <br><br><label for="picture">Image :</label>
+    <input type="image" name="image" required>
+
+    <br><br>
+
+    <label for="content">Content :</label>
+    <textarea id="content" name="contenu"></textarea><br><br>
+
+    <button type="submit">Soumettre</button>
+</div>
 
 
+</form>
 
 
+<br><br>
 
+<h2>Creat comment</h2>
 
+<div>
+
+    <br><br><label for="pseudo">Pseudo :</label>
+    <input type="text" name="pseudo" required>
+
+    <br><br>
+
+    <label for="content">Content :</label>
+    <textarea id="content" name="contenu"></textarea><br><br>
+
+    <button type="submit">Soumettre</button>
+</div>
 
 <?php
 include_once("component/footer.php");
